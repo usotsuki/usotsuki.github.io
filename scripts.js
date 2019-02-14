9b112fe69dfbc9b17cfcd3ced14624197da04add
usotsuki <47646156+usotsuki@users.noreply.github.com>
2019-02-14 17:28:04 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,11 +10,31 @@ request.onload = function () {
 	var data = JSON.parse(this.response);
 
 	if (request.status >= 200 && request.status < 400) {
-		console.log("Status: " + data.status);
+		/*console.log("Status: " + data.status);
 		console.log("Uptime: " + data.time.duration.formated);
+
+		const card = document.createElement('div');
+		card.setAttribute('class', 'card');
+
+		const h1 = document.createElement('h1');
+		h1.textContent = movie.title;
+
+		const p = document.createElement('p');
+		movie.description = movie.description.substring(0, 300);
+		p.textContent = `${movie.description}...`;
+
+		container.appendChild(card);
+		card.appendChild(h1);
+		card.appendChild(p);*/
+
+		const errorMessage = document.createElement('marquee');
+		errorMessage.textContent = `Please try again later.`;
+		app.appendChild(errorMessage);
 	}
 	else {
-		console.log('error');
+		const errorMessage = document.createElement('marquee');
+		errorMessage.textContent = `Please try again later.`;
+		app.appendChild(errorMessage);
 	}
 }
 
