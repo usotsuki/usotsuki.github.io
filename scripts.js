13ab27e794ded0b63391067010673babb39010c9
usotsuki <47646156+usotsuki@users.noreply.github.com>
2019-02-14 17:37:15 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
 const app = document.getElementById('root');
 
+const logo = document.createElement('img');
+logo.src = 'logo.png';
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 
