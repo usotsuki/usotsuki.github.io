76e81e866cacf64c0a2b2fe846ac41226a567100
usotsuki <47646156+usotsuki@users.noreply.github.com>
2019-02-14 17:34:20 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,26 +12,22 @@ request.onload = function () {
 	var data = JSON.parse(this.response);
 
 	if (request.status >= 200 && request.status < 400) {
-		/*console.log("Status: " + data.status);
-		console.log("Uptime: " + data.time.duration.formated);
+		//console.log("Status: " + data.status);
+		//console.log("Uptime: " + data.time.duration.formated);
 
 		const card = document.createElement('div');
 		card.setAttribute('class', 'card');
 
 		const h1 = document.createElement('h1');
-		h1.textContent = movie.title;
+		h1.textContent = "Status: " + data.status;
 
 		const p = document.createElement('p');
-		movie.description = movie.description.substring(0, 300);
-		p.textContent = `${movie.description}...`;
+		data.time.duration.formated = "Uptime: " + data.time.duration.formated.substring(0, 300);
+		p.textContent = `${data.time.duration.formated}...`;
 
 		container.appendChild(card);
 		card.appendChild(h1);
-		card.appendChild(p);*/
-
-		const errorMessage = document.createElement('marquee');
-		errorMessage.textContent = `Please try again later.`;
-		app.appendChild(errorMessage);
+		card.appendChild(p);
 	}
 	else {
 		const errorMessage = document.createElement('marquee');
